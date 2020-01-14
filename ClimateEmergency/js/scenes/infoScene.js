32e44ea255cfaf82be3dcdb0a14bfb99f1f167a5
rykent1 <rykenthompson@icloud.com>
2020-01-14 17:49:39 +0000

    play again button

--- ClimateEmergency/js/scenes/infoScene.js
+++ ClimateEmergency/js/scenes/infoScene.js
@@ -26,12 +26,39 @@ var infoScene = new Phaser.Class({
         this.background.setScale(0.65);
 
 
-        this.info = this.add.sprite(300,200, 'info');
+        this.info = this.add.sprite(320,150, 'info');
         this.info.visible = false;
+        this.again = this.add.sprite(320, 300, 'again');
+        this.again.visible = false;
+        this.again.setInteractive();
+        
+        this.again.on('pointerdown', function(pointer){
+            win = false;
+            timeLeft = 30;
+            switch (dif) {
+                case 1:
+                    timeAtStart = 35;
+                    break;
+                case 2:
+                    timeAtStart = 40;
+                    break;
+                case 3:
+                    timeAtStart = 35;
+                    break;
+            }
+            winAmount = 30;
+            isInfo = false;
+            isIntro = false;
+            trashPickedUp = 0;
+            trashArray = [];
+            start = true;
+            this.scene.start('pickupScene');
+        }, this);
 
         this.input.on('pointerdown', function(pointer) {
             this.winlose.visible = false;
             this.info.visible = true;
+            this.again.visible = true;
         }, this);
     },
 
